c22ee43ad9badc866283ee5fd670bdb2b7958c78
Bálint Hejner <hejnerbalint@gmail.com>
2024-04-06 15:45:18 +0200

    headers added for solving cors issues

--- products.php
+++ products.php
@@ -5,6 +5,9 @@
 
     session_start();
     header("Content-Type: application/json");
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type, X-Requested-with, Authorization");
 
     if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_SESSION["username"]))
     {
